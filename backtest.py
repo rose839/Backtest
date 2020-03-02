01e895d73f9607ce13de6dba54320bb31818bdbb
rose839 <617418745@qq.com>
2020-03-03 00:19:28 +0800

    add broker class

--- backtest.py
+++ backtest.py
@@ -32,3 +32,59 @@ class Backtest(object):
         assert_msg(issubclass(broker_type,   ExchangeAPI), "broker_type is not a ExchangeAPI type")
         assert_msg(isinstance(commission,    Number),      "commission is not a Number type")
 
+        data = data.copy(False)
+
+        if 'Volume' not in data:
+            data['Volume'] = np.nan
+
+        # verify OHLC data format
+        assert_msg(len(data.columns & {'Open', 'High', 'Low', 'Close', 'Volume'}) == 5,
+                   ("Data format not correct, need those columns folowing at least: "
+                    "'Open', 'High', 'Low', 'Close'"))
+
+        # sort as date
+        if not data.index.is_monotonic_increasing:
+            data = data.sort_index()
+
+        # init object
+        self._data = data
+        self._broker = broker_type(data, cash, commission)
+        self._strategy = strategy_type(self._broker, self._data)
+        self._results = None
+
+    def run(self) -> pd.Series:
+        """
+        Run the backtest. Returns `pd.Series` with results and statistics.
+        """
+        strategy = self._strategy
+        broker   = self._broker
+
+        # strategy init
+        strategy.init()
+
+        # set backtest start and end position in data
+        start = 100
+        end = len(self._data)
+
+        # backtest main loop, update market state, and execute strategy
+        for i in range(start, end):
+            # update market state first, then execute strategy
+            broker.next(i)
+            strategy.next(i)
+
+        # after execute strategy, compute result and return
+        self._results = self._compute_result(broker)
+        return self._results
+
+    def _compute_result(self, broker):
+        s = pd.Series()
+        s['initial value'] = broker.initial_cash
+        s['ending value']  = broker.market_value
+        s['profit']        = broker.market_value - broker.initial_cash
+        return s
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
---
+++ exchangeAPI.py
@@ -0,0 +1,59 @@
+from utils import assert_msg
+
+class ExchangeAPI(object):
+    def __init__(self, data, cash, commission):
+        """
+        :param data:       pd.DataFrame  market data
+        :param cash:       float         initial cash
+        :param commission: float         commission for every trade
+        :return:
+        """
+
+        assert_msg(0 < cash, 'Initial cash must be greater than 0, input cash: {}'.format(cash))
+        assert_msg(0 <= commission <= 0.05, "Resonable commission won't be less than 0.05, input: {}".format(commission))
+
+        self._initial_cash = cash
+        self._data = data
+        self._commission = commission
+        self._position = 0 # current account position
+        self._cash = cash  # current cash in account
+        self._cursor = 0   # corrent position in data
+
+        @property
+        def cash(self):
+            return self._cash
+
+        @property
+        def position(self):
+            return self._position
+
+        @property
+        def initial_cash(self):
+            return self._initial_cash
+
+        @property
+        def market_value(self):
+            return self._cash + self._position * self.current_price
+
+        @property
+        def current_price(self):
+            return self._data.Close[self._cursor]
+
+        def buy(self):
+            """
+            Buying in as market value using spare cash in account
+            :return:
+            """
+            self._position = float(self._cash * (1 - self._commission) / self.current_price)
+            self._cash = 0.0
+
+        def sell(self):
+            """
+            Selling out all position
+            :return:
+            """
+            self._cash += float(self._position * self.current_price * (1 - self._commission))
+            self._position = 0.0
+
+        def next(self, tick):
+            self._cursor = tick
